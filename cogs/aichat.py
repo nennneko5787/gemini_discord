2565ab615d86d23172f4f95900e9d67259a74e65
Nennneko5787 <nennneko5787@gmail.com>
2024-07-21 19:58:41 +0900

    First commit

---
+++ cogs/aichat.py
@@ -0,0 +1,140 @@
+import json
+import os
+import traceback
+from collections import defaultdict
+
+import aiohttp
+import asyncpg
+import discord
+from discord.ext import commands
+
+from .utils import Gemini
+
+if os.path.isfile(".env"):
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+
+class AIChatCog(commands.Cog):
+    def __init__(self, bot: commands.Bot, *, proxies: dict):
+        self.bot: commands.Bot = bot
+        self.chatHistories: dict = defaultdict(list)
+        self.apiKeys = [
+            os.getenv("gemini0"),
+            os.getenv("gemini1"),
+            os.getenv("gemini2"),
+        ]
+        self.proxies = [
+            f"{proxy.get('protocols', [])[0]}://{proxy.get('ip')}:{proxy.get('port')}"
+            for proxy in proxies.get("data", [])
+        ]
+        print("AIChatCog loaded")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.tree.sync()
+        self.pool: asyncpg.Pool = await asyncpg.create_pool(
+            os.getenv("dsn"), statement_cache_size=0
+        )
+        async with self.pool.acquire() as conn:
+            rows = await conn.fetch("SELECT * FROM users")
+            for row in rows:
+                self.chatHistories[row["id"]] = json.loads(row["data"])
+        print("Database connected and chat histories loaded.")
+
+    @commands.hybrid_command(
+        name="delmsghistory", description="Geminiとのメッセージ履歴を削除します。"
+    )
+    async def delmsghistory(self, ctx: commands.Context):
+        await ctx.defer(ephemeral=True)
+        self.chatHistories[ctx.author.id] = []
+        try:
+            async with self.pool.acquire() as conn:
+                await conn.execute(
+                    """
+                    INSERT INTO users (id, data)
+                    VALUES ($1, $2)
+                    ON CONFLICT (id)
+                    DO UPDATE SET
+                        data = EXCLUDED.data
+                    """,
+                    ctx.author.id,
+                    json.dumps(self.chatHistories[ctx.author.id]),
+                )
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send("エラーが発生しました。", ephemeral=True)
+        else:
+            await ctx.send("メッセージ履歴を消去しました。", ephemeral=True)
+
+    def splitContent(self, content, maxLength=2000):
+        parts = []
+        while len(content) > maxLength:
+            splitPoint = maxLength
+            while splitPoint > 0 and not content[splitPoint].isspace():
+                splitPoint -= 1
+            if splitPoint == 0:
+                splitPoint = maxLength
+            part = content[:splitPoint].strip()
+            parts.append(part)
+            content = content[splitPoint:].strip()
+        if content:
+            parts.append(content)
+        return parts
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if (
+            message.channel.id != 1264471909903368338
+            or message.author.bot
+            or message.author.system
+        ):
+            return
+        async with message.channel.typing():
+            try:
+                content = await Gemini.stream(
+                    message.clean_content,
+                    apiKeys=self.apiKeys,
+                    history=self.chatHistories[message.author.id],
+                    proxies=None,
+                )
+            except Exception as e:
+                traceback.print_exc()
+                await message.reply("Error", delete_after=5)
+                await message.delete(delay=5)
+                return
+            self.chatHistories[message.author.id].append(
+                {"parts": [{"text": message.clean_content}], "role": "user"}
+            )
+            self.chatHistories[message.author.id].append(
+                {"parts": [{"text": content}], "role": "model"}
+            )
+            contents = self.splitContent(content)
+            for c in contents:
+                await message.reply(c)
+
+            try:
+                async with self.pool.acquire() as conn:
+                    await conn.execute(
+                        """
+                        INSERT INTO users (id, data)
+                        VALUES ($1, $2)
+                        ON CONFLICT (id)
+                        DO UPDATE SET
+                            data = EXCLUDED.data
+                        """,
+                        message.author.id,
+                        json.dumps(self.chatHistories[message.author.id]),
+                    )
+            except Exception as e:
+                traceback.print_exc()
+
+
+async def setup(bot: commands.Bot):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc"
+        ) as response:
+            proxies = await response.json()
+            await bot.add_cog(AIChatCog(bot, proxies=proxies))
---
+++ cogs/utils/gemini.py
@@ -0,0 +1,105 @@
+import asyncio
+import json
+import traceback
+from typing import List
+import random
+
+import aiohttp
+
+
+class Gemini:
+    @classmethod
+    async def stream(
+        cls,
+        content: str,
+        apiKeys: List[str],
+        history: List[dict] = None,
+        model: str = "gemini-1.5-pro",
+        proxies: List[str] = None,
+    ) -> str:
+        count = 0
+        maxcount = len(apiKeys)
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+
+        if not history:
+            history = list()
+
+        contents = history
+        contents.append(
+            {
+                "parts": [
+                    {
+                        "text": content,
+                    },
+                ],
+                "role": "user",
+            }
+        )
+
+        data: dict = {
+            "contents": contents,
+            "safetySettings": [
+                {
+                    "category": "HARM_CATEGORY_HATE_SPEECH",
+                    "threshold": "BLOCK_NONE",
+                },
+                {
+                    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                    "threshold": "BLOCK_NONE",
+                },
+                {
+                    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                    "threshold": "BLOCK_NONE",
+                },
+                {
+                    "category": "HARM_CATEGORY_HARASSMENT",
+                    "threshold": "BLOCK_NONE",
+                },
+            ],
+        }
+
+        async with aiohttp.ClientSession() as session:
+            while count < maxcount:
+                if proxies:
+                    proxy = random.choice(proxies)
+                else:
+                    proxy = None
+                apiKey = apiKeys[count]
+                try:
+                    async with session.post(
+                        f"https://generativelanguage.googleapis.com/v1beta/models/{model}:streamGenerateContent?key={apiKey}",
+                        json=data,
+                        headers=headers,
+                        proxy=proxy,
+                    ) as response:
+                        response.raise_for_status()
+                        byteList = []
+                        async for line in response.content:
+                            byteList.append(line)
+                        data = b"".join(byteList).decode()
+                        jsonData = json.loads(data)
+                        responseList = []
+                        for content in jsonData:
+                            responseList.append(
+                                content.get("candidates", [])[0]
+                                .get("content", {})
+                                .get("parts")[0]
+                                .get("text")
+                            )
+                        return "".join(responseList)
+                except:
+                    traceback.print_exc()
+                    count += 1
+            response.raise_for_status()
+
+
+async def main():
+    print("result", await Gemini.stream("こんにちは"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
