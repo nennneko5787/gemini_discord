008d380d6403f942b47be2322d25d2415bd29894
ねんねこ <nennneko5787@gmail.com>
2024-09-06 01:25:36 +0900

    aichat.py を更新

--- cogs/aichat.py
+++ cogs/aichat.py
@@ -23,7 +23,9 @@ class AIChatCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
         self.loaded = False
+        self.model: dict = defaultdict(str)
         self.chatHistories: dict = defaultdict(list)
+        self.chatHistoriesNSFW: dict = defaultdict(list)
         self.apiKeys = [GeminiAPIKey(os.getenv(f"gemini{i}")) for i in range(20)]
         print(len(self.apiKeys))
         self.waitList = []
@@ -34,14 +36,42 @@ class AIChatCog(commands.Cog):
         await self.bot.tree.sync()
         rows = await Database.pool.fetch("SELECT * FROM users")
         for row in rows:
+            self.model[row["id"]] = row["model"]
             self.chatHistories[row["id"]] = json.loads(row["data"])
+            self.chatHistoriesNSFW[row["id"]] = json.loads(row["data_nsfw"])
         print("Database connected and chat histories loaded.")
         self.loaded = True
 
     @commands.hybrid_command(
-        name="delmsghistory", description="Geminiとのメッセージ履歴を削除します。"
+        name="model", description="Geminiのモデルを変更します。"
     )
-    async def delmsghistory(self, ctx: commands.Context):
+    async def clear(self, ctx: commands.Context, model: str):
+        await ctx.defer(ephemeral=True)
+        if not self.loaded:
+            await ctx.send("データベースの初期化が終わっていません", ephemeral=True)
+        self.model[ctx.author.id] = model
+        try:
+            await Database.pool.execute(
+                """
+                    INSERT INTO users (id, model)
+                    VALUES ($1, $2)
+                    ON CONFLICT (id)
+                    DO UPDATE SET
+                        model = EXCLUDED.model
+                """,
+                ctx.author.id,
+                model,
+            )
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send("エラーが発生しました。", ephemeral=True)
+        else:
+            await ctx.send("モデルを変更しました。", ephemeral=True)
+
+    @commands.hybrid_command(
+        name="clear", description="Geminiとのメッセージ履歴を削除します。"
+    )
+    async def clear(self, ctx: commands.Context):
         await ctx.defer(ephemeral=True)
         if not self.loaded:
             await ctx.send("データベースの初期化が終わっていません", ephemeral=True)
@@ -57,6 +87,32 @@ class AIChatCog(commands.Cog):
                 """,
                 ctx.author.id,
                 json.dumps(self.chatHistories[ctx.author.id]),
+            )
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send("エラーが発生しました。", ephemeral=True)
+        else:
+            await ctx.send("メッセージ履歴を消去しました。", ephemeral=True)
+
+    @commands.hybrid_command(
+        name="clearnsfw", description="NSFW Geminiとのメッセージ履歴を削除します。"
+    )
+    async def clearnsfw(self, ctx: commands.Context):
+        await ctx.defer(ephemeral=True)
+        if not self.loaded:
+            await ctx.send("データベースの初期化が終わっていません", ephemeral=True)
+        self.chatHistoriesNSFW[ctx.author.id] = []
+        try:
+            await Database.pool.execute(
+                """
+                    INSERT INTO users (id, data_nsfw)
+                    VALUES ($1, $2)
+                    ON CONFLICT (id)
+                    DO UPDATE SET
+                        data = EXCLUDED.data
+                """,
+                ctx.author.id,
+                json.dumps(self.chatHistoriesNSFW[ctx.author.id]),
             )
         except Exception as e:
             traceback.print_exc()
@@ -92,6 +148,10 @@ class AIChatCog(commands.Cog):
         if message.author.id in self.waitList:
             await message.reply("メッセージ生成を待つ必要があります。")
             return
+        if message.channel.id == 1264471909903368338:
+            history = self.chatHistories[message.author.id]
+        else:
+            history = self.chatHistoriesNSFW[message.author.id]
         self.waitList.append(message.author.id)
         try:
             async with message.channel.typing():
@@ -100,18 +160,19 @@ class AIChatCog(commands.Cog):
                     content = await Gemini.chat(
                         message.clean_content,
                         apiKeys=self.apiKeys,
-                        history=self.chatHistories[message.author.id],
+                        history=history,
                         files=message.attachments,
                         proxies=None,
+                        model=self.model[message.author.id]
                     )
                 except Exception as e:
                     traceback.print_exc()
                     await message.reply("Error")
                     return
-                self.chatHistories[message.author.id].append(
+                history.append(
                     {"parts": [{"text": message.clean_content}], "role": "user"}
                 )
-                self.chatHistories[message.author.id].append(
+                history.append(
                     {"parts": [{"text": content}], "role": "model"}
                 )
                 contents = self.splitContent(content)
@@ -120,15 +181,15 @@ class AIChatCog(commands.Cog):
     
             try:
                 await Database.pool.execute(
-                    """
-                        INSERT INTO users (id, data)
+                    f"""
+                        INSERT INTO users (id, {"data " if message.channel.id == 1264471909903368338 else "data_nsfw"})
                         VALUES ($1, $2)
                         ON CONFLICT (id)
                         DO UPDATE SET
                             data = EXCLUDED.data
                     """,
                     message.author.id,
-                    json.dumps(self.chatHistories[message.author.id]),
+                    json.dumps(history),
                 )
             except Exception as e:
                 traceback.print_exc()
